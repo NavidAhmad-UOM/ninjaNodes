ff5f5ca0dda36135b4a97a0c634d8e8b7bfdcf14
NavidAhmad <toengnrnaveed@gmail.com>
2020-01-15 13:39:00 +0500

    mlab connectivity

--- app.js
+++ app.js
@@ -2,7 +2,14 @@ const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const app =  express();
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
+//connect to mLab database
+mongoose.connect('mongodb+srv://test:test@cluster0-z4aus.mongodb.net/test?retryWrites=true&w=majority',
+{ useNewUrlParser: true,useUnifiedTopology: true });
+mongoose.connection.once('open',()=>{
+    console.log('connected to mongoose database');
+})
 
 app.use("/graphql",graphqlHTTP({
     schema,
